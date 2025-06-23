683f97fd7440cc3d9a9739e262d2a98214556c8d
Lucas Carioca <lucascarioca97@gmail.com>
2025-06-23 18:38:12 -0300

    project setup

---
+++ components/ThemeButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+	get() {
+		return colorMode.value === 'dark'
+	},
+	set(_isDark) {
+		colorMode.preference = _isDark ? 'dark' : 'light'
+	},
+})
+</script>
+
+<template>
+	<ClientOnly v-if="!colorMode?.forced">
+		<UButton
+			:icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+			color="neutral"
+			variant="ghost"
+			@click="isDark = !isDark"
+		>
+			<span className="sr-only">Toggle theme</span>
+		</UButton>
+
+		<template #fallback>
+			<USkeleton class="size-8" />
+		</template>
+	</ClientOnly>
+</template>
