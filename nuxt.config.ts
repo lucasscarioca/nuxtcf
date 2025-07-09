2a29ba19a905b46e94a613ce90e4a569392c8fa0
Lucas Carioca <lucascarioca97@gmail.com>
2025-07-09 18:32:43 -0300

    chore: upgrade to nuxt v4 compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
